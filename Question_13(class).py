ea3d68504c98f47432b59c92347ddeb960957d8b
Shivkpra <101056510+Shivkpra@users.noreply.github.com>
2022-04-16 18:44:16 +0530

    Update Question_13(class).py

--- Question_13(class).py
+++ Question_13(class).py
@@ -1,21 +1,21 @@
 #String to Integer and Vice Versa
 
 
-class convert:
-  def __init__(self,a,b):
+class convert:                                    #creating class
+  def __init__(self,a,b):                         #constructor
       self.a=a
       self.b=b
 
 
-  def Convert(self,a,b):
+  def Convert(self,a,b):                         #function declaration
       c = int(a)
       print(c)
-      print(type(c))
+      print(type(c))                             #type of c is printed
       d = str(b)
       print(d)
-      print(type(d))
+      print(type(d))                             #type of d is printed 
 
 a = input("Enter the string")
 b = int(input("Enter the integer"))
-obj=convert(a,b)
+obj=convert(a,b)                                 #creating object for class
 obj.Convert(a,b)
