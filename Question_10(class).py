edf8409b788be0588095933721233f08bc611b16
Shivkpra <101056510+Shivkpra@users.noreply.github.com>
2022-04-16 18:23:42 +0530

    Update Question_10(class).py

--- Question_10(class).py
+++ Question_10(class).py
@@ -3,22 +3,22 @@ Create a function that takes the number of wins, draws and losses and calculates
 wins get 3 points
 draws get 1 point
 losses get 0 points'''
-class total_points:
-    def __init__(self,win,draw,loss):
+class total_points:                                                                         # creating class
+    def __init__(self,win,draw,loss):                                                       #Constructor
         self.win=win
         self.draw=draw
         self.loss=loss
 
-    def Points(self,win: object, draw: object, loss: object) -> object:
+    def Points(self,win: object, draw: object, loss: object) -> object:                     #Function declaration
         wins = win * 3
         draws = draw * 1
         losses = 0
-        points = wins + draws + losses
+        points = wins + draws + losses                                                      #calculate total points of the teams
         return points
 
 win = int(input('Enter the  number of win'))
 draw = int(input('Enter the  number of draws'))
 loss = int(input('Enter the  number of loss'))
-obj=total_points(win,draw,loss)                 # creating object for class
+obj=total_points(win,draw,loss)                                                             # creating object for class
 print('The points of the team is:',obj.Points(win, draw, loss))
 
