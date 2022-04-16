5440599012515b91ce5a6e7604febc4f4eb75587
Shivkpra <101056510+Shivkpra@users.noreply.github.com>
2022-04-16 18:45:49 +0530

    Update Question_13(function).py

--- Question_13(function).py
+++ Question_13(function).py
@@ -1,12 +1,12 @@
 #String to Integer and Vice Versa
 
-def type_coversion(a,b):
+def type_coversion(a,b):            #function declaration
     c = int(a)
     print(c)
-    print(type(c))
+    print(type(c))                  #type of c is print
     d=str(b)
     print(d)
-    print(type(d))
+    print(type(d))                  #type of d is print            
 a=input("Enter the string")
 b=int(input("Enter the integer"))
 type_coversion(a,b)                  #function call
