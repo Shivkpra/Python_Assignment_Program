6e87aaccaf5e5ab634c23eaafb519ad0db4ceefa
Shivkpra <101056510+Shivkpra@users.noreply.github.com>
2022-04-16 18:28:00 +0530

    Update Question_11(class).py

--- Question_11(class).py
+++ Question_11(class).py
@@ -6,21 +6,21 @@ pigs = 4 legs
 The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals.
 '''
 
-class Farm:
-  def __init__(self,ch_num,co_num,pi_num):
+class Farm:                                                                 #creating class
+  def __init__(self,ch_num,co_num,pi_num):                                  #Construtor
       self.ch_num=ch_num
       self.co_num=co_num
       self.pi_num=pi_num
 
-  def total_legs( self,ch, co, pi):
+  def total_legs( self,ch, co, pi):                                        #functiom declaration
       ch_legs = ch_num * 2
       co_legs = co_num * 4
       pi_legs = pi_num * 4
-      total_leg = ch_legs + co_legs + pi_legs
+      total_leg = ch_legs + co_legs + pi_legs                             #calculate total number of leg in farm
       return total_leg
 
 ch_num = int(input('Enter the  number of chickens in farm'))
 co_num = int(input('Enter the  number of cows in farm'))
 pi_num = int(input('Enter the  number of pig in farm'))
-obj=Farm(ch_num,co_num,pi_num)
+obj=Farm(ch_num,co_num,pi_num)                                            #creating object 
 print('Total legs of the animal which are present in farm is', obj.total_legs(ch_num, co_num, pi_num))
