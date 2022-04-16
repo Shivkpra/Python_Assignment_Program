00f741ee458f2d7cbbc42e6eb908c18174cb8bac
Shivkpra <101056510+Shivkpra@users.noreply.github.com>
2022-04-16 18:29:19 +0530

    Update Question_11(input and output).py

--- Question_11(input and output).py
+++ Question_11(input and output).py
@@ -12,5 +12,5 @@ pigs_num=int(input('Enter the  number of pig in farm'))
 chickens_legs=chickens_num* 2
 cows_legs=cows_num* 4
 pigs_legs=pigs_num* 4
-total_legs=chickens_legs+cows_legs+pigs_legs
+total_legs=chickens_legs+cows_legs+pigs_legs                                  # calculating legs of animal in farm 
 print('Total legs of the animal which are present in farm is',total_legs)
