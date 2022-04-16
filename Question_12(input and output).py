f84e798cb3f93271ab13ad84897503a730cb0159
Shivkpra <101056510+Shivkpra@users.noreply.github.com>
2022-04-16 18:41:40 +0530

    Update Question_12(input and output).py

--- Question_12(input and output).py
+++ Question_12(input and output).py
@@ -1,13 +1,13 @@
 # Convert Hours and Minutes into Seconds
-hr=int(input('Enter the hours'))
-if(hr<=24):
-    hour=hr*60*60;
+hr=int(input('Enter the hours')) 
+if(hr<=24):                                                #checking hr is less than 24 or not
+    hour=hr*60*60;                                         #converting hr is second
 else:
     print("invalid value")
 min=int(input('Enter the mintues'))
-if(min<=30):
-    minutes=min*60
+if(min<=30):                                              #checking  min is less than 30 or not
+    minutes=min*60                                        #converting min to second
 else:
     print('invalid value')
-seconds=hour+minutes
+seconds=hour+minutes                                      #sum of all seconds
 print(f"In {hr} hours and {min} minutes have {seconds}")
