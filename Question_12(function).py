7a16ab265691239e8d21bee3ca636e93d23f5db1
Shivkpra <101056510+Shivkpra@users.noreply.github.com>
2022-04-16 18:38:33 +0530

    Update Question_12(function).py

--- Question_12(function).py
+++ Question_12(function).py
@@ -1,19 +1,19 @@
 # Convert Hours and Minutes into Seconds
 
 
-def seconds(hr ,min):
-    if (hr <= 24):
-        hour = hr * 60 * 60;
+def seconds(hr ,min):                                     #function declaration
+    if (hr <= 24):                                        # checking hr is less than 24 or or not
+        hour = hr * 60 * 60;                              #converting hr in seconds
     else:
         print("invalid value")
 
-    if (min <= 30):
-        minutes = min * 60
+    if (min <= 30):                                       #checking min is less than 30 or not
+        minutes = min * 60                                #converting min in seconds
     else:
         print('invalid value')
-    second = hour + minutes
+    second = hour + minutes                               #sum of all seconds
     return second
 
 min = int(input('Enter the mintues'))
 hr = int(input('Enter the hours'))
-print("total seconds are",seconds(hr,min))   # function  call in  print statement
+print("total seconds are",seconds(hr,min))               # function  call in  print statement
